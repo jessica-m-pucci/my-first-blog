c8abcb174f9958d81a1fdc59c04b673e9a3bc652
JessMPucci <jess@pucci.me>
2026-05-30 12:26:58 +0200

    fix: settings.py - ALLOWED_HOSTS fix

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-%v@+e1))g3y&+no-x&t)am48qyt!jufn92ijk$j2l%gn+aj$$l
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'pythonanywhere.com']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '.pythonanywhere.com']
 
 
 # Application definition
